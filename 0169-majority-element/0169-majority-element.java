067ad304a8149ab1a7b8775a975f243bb479df8c
Richards <96327922+Richards-Richie@users.noreply.github.com>
2023-07-16 16:55:51 +0530

    Time: 17 ms (16.73%), Space: 47 MB (75.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -6,13 +6,9 @@ class Solution {
         }
         HashMap <Integer,Integer>hm=new HashMap<>();
         for(int i=0;i<nums.length;i++){
-            if(hm.containsKey(nums[i])){
-                hm.put(nums[i],hm.get(nums[i])+1);
-                if(hm.get(nums[i]) > (nums.length/2)){
-                    ans = nums[i];
-                }
-            }else{
-                hm.put(nums[i],1);
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+            if(hm.get(nums[i]) > (nums.length/2)){
+                ans=nums[i];
             }
         }
         return ans;
